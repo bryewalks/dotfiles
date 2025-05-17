a25d2ca76a35bcd39d45d96fa0f1f6959d546803
Brye Walker <bryewalks@gmail.com>
2025-05-17 01:41:36 -0500

    neovim config

--- neovim/.config/nvim/lua/vim-options.lua
+++ neovim/.config/nvim/lua/vim-options.lua
@@ -23,6 +23,9 @@ vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 
+-- Rebind c-i to itself to distinguish between c-i and tab
+vim.keymap.set('n', '<c-i>', '<c-i>')
+
 vim.diagnostic.config({
   virtual_text = true,
   signs = true,

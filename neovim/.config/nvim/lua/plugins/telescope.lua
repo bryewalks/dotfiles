2c70350934c6cb9cb79480ff519c432d3f1d67e2
Brye Walker <bryewalks@gmail.com>
2025-05-19 02:20:21 -0500

    More finds

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -8,7 +8,8 @@ return {
 			vim.keymap.set("n", "<leader>f", "", { desc = "Find" })
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
 			vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Find marks" })
-      vim.keymap.set("n", "<leader>fb", builtin.git_branches, { desc = "Find git branches" })
+      vim.keymap.set("n", "<leader>ft", builtin.git_branches, { desc = "Find git branches" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
 		end,
 	},

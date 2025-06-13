eefae608fbbe0c617ff3db72ad488b28e208811d
Brye Walker <bryewalks@gmail.com>
2025-06-13 18:48:44 -0500

    Fix neovim recording

--- neovim/.config/nvim/lua/plugins/noice.lua
+++ neovim/.config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,19 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  opts = {},
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "rcarriga/nvim-notify",
-    }
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		local noice = require("noice")
+		noice.setup({
+			routes = {
+				{
+					view = "notify",
+					filter = { event = "msg_showmode" },
+				},
+			},
+		})
+	end,
 }

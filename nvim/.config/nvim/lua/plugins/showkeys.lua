3de47a232adadc9df5d2531697bd5efed824f865
Brye Walker <bryewalks@gmail.com>
2025-06-27 18:09:25 -0500

    Show keys

---
+++ nvim/.config/nvim/lua/plugins/showkeys.lua
@@ -0,0 +1,11 @@
+return {
+  "nvzone/showkeys",
+  cmd = "ShowkeysToggle",
+  keys = {
+    { "<leader>bs", "<cmd>ShowkeysToggle<cr>", desc = "Show Keys" },
+  },
+  opts = {
+    position = "top-right",
+    show_count = true,
+  },
+}
